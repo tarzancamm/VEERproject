f3ea10662e36fef9a2d2cc503638a0f3765dbb35
Cameron Miner <cameronsminer@gmail.com>
2022-11-10 16:43:04 -0700

    Added start section under script in package.json

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const { DATABASE_URL } = process.env;
+const { CONNECTION_STRING } = process.env;
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
