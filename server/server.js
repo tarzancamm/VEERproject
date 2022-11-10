8aa6f81aec43b68a9f0359792b6ca7f2bc8ed855
Cameron Miner <cameronsminer@gmail.com>
2022-11-10 15:44:50 -0700

    Updating code from local host to heroku host

--- server/server.js
+++ server/server.js
@@ -10,11 +10,56 @@ const { seed, getCountries, submitAdventure, getAdventure } = require('./control
 app.use(express.json())
 app.use(cors())
 
+
 // Seed DB
 app.post('/seed', seed)
 
 // Endpoints connecting HTML, CSS and JS
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
+app.get('/style', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/style.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.js'))
+})
+
+app.get('/add-adventure', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/add-adventure.html'))
+})
+
+app.get('/adventure/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/add-adventure.js'))
+})
+
+
+// Endpoints connecting images
+app.get('image-one', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/netherlands.png"))
+})
+
+app.get('image-two', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/spain.png"))
+})
+
+app.get('image-three', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/france.png"))
+})
+
+app.get('image-four', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/indonesia.png"))
+})
+
+app.get('image-five', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/england.png"))
+})
+
+app.get('image-six', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style_resources/argentina.png"))
+})
 
 
 // Endpoints
